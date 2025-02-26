7c70abb48c488d9a3a6914e86d0c8ae55601f44c
Davidnna <davedozie1@gmail.com>
2025-02-27 00:59:00 +0100

    Correct Arrangement

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+const saveEl = document.getElementById("save-el")
+const countEl = document.getElementById("count-el")
 let count = 0
 
+// Count
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+// Save Counted
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
